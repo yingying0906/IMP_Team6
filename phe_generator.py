aea98be82d2d4bb9012a87d31b2d67d3f2995730
yingying0906 <popo090699@gmail.com>
2022-11-24 17:31:59 +0800

    Cryptographic splitting added
    - split the encrypted_number into n shares
    - pick m shares
    - combine the m shares

--- phe_generator.py
+++ phe_generator.py
@@ -3,6 +3,9 @@
 # $ pip install phe
 
 from phe import paillier
+import shamirs
+import splitting
+import random
 
 def getKey(): # get public key and private key from pyphe package
     public_key, private_key = paillier.generate_paillier_keypair()
@@ -21,15 +24,33 @@ def getDecryptedNumber(encrypted_number_list, public_key, private_key): # get de
 
 
 if __name__=='__main__':
-    secret_number_list = [1, 2, 3] # change number you want to test here
+    secret_number_list = [55, 77, 36698] # change number you want to test here
     public_key, private_key = getKey()
     encrypted_number_list = getEncryptedNumber(secret_number_list) # the type of each item in encrypted_number_list is 'int'
 
     print(f'there are {len(encrypted_number_list)} numbers and their type is {type(encrypted_number_list[0])}')
 
-    # Do data splitting here
+    # Split each secret number
+    shares_list = []
+    combine_list = []
+    for num in encrypted_number_list:
+        shares_list.append(shamirs.shares(num, quantity=6, threshold=4, modulus=2**9689-1))
+    print(f'The secret number is splitted into 6 shares with threshold 4')
 
-    decrypted_number_list = getDecryptedNumber(encrypted_number_list, public_key, private_key) # the type of each item in encrypted_number_list should be 'int'
+    # Pick random chunks in each secret number
+    pick_list = []
+    for num in shares_list:
+        pick_list.append(random.sample(num, 4))
+    print(f'Partial shares are picked')
+
+    # Combine chunks in each secret number
+    combine_list = []
+    for num in pick_list:
+        combine_list.append(shamirs.interpolate(num))
+    print(f'The shares are combined')
+
+    # Decrypted
+    decrypted_number_list = getDecryptedNumber(combine_list, public_key, private_key) # the type of each item in encrypted_number_list should be 'int'
 
     print(f'there are {len(decrypted_number_list)} numbers and the value is below:')
     for number in decrypted_number_list:
